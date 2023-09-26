ff9e17da56a87841acbe66b81ce596839435edff
XQZmeSIR <rys2000lan7733@mail.ru>
2023-09-26 22:41:11 +0300

    Finished modul programm 'geometry'.

--- Chapter 5: Functions/5.9,МатМодуль_math.py
+++ Chapter 5: Functions/5.9,МатМодуль_math.py
@@ -1,3 +1,25 @@
 import math
-result = math.sqrt(60000)
-print(f"{result:,.2f}")
+# result = math.sqrt(60000)      ###@@ .sqrtКвадратный корень
+# print(f"{result:,.2f}")
+
+# def main():
+    # a = float(input('A: '))
+    # b = float(input('B: '))
+# 
+    # c = math.hypot(a, b)         ###@@ .hypot  Гипотенуза
+# 
+    # print(f"The lenght of hypotenuza is: {c}")
+# 
+# 
+# main()
+
+# def main():
+#     pi_numb = math.pi              ###@@ .pi  Номер Пи;   .e номер е
+#     print('The pi number is:', pi_numb)
+
+# main()
+
+# def kv():
+#     grad = math.radians(45)          ###@@ .radians   Угол из градусов в радианы
+#     print(grad)
+# kv()
---
+++ Chapter 5: Functions/rectangle.py
@@ -0,0 +1,8 @@
+import math
+
+def area(width, lenght):
+    return width * lenght
+
+
+def perimeter(width, lenght):
+    return 2 * (width + lenght)
