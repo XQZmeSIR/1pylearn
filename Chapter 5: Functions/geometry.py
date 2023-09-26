add6f43752f6ba077bf6956d0590ef1e73bf4d78
XQZmeSIR <rys2000lan7733@mail.ru>
2023-09-26 22:53:48 +0300

    small changes with indentions

--- Chapter 5: Functions/geometry.py
+++ Chapter 5: Functions/geometry.py
@@ -13,23 +13,38 @@ def main():
     while choice != QUIT_CHOICE:
         display_menu()
         choice = int(input("Enter your choice: "))
+            print()
+            print()
+
 
         if choice == AREA_CIRCLE_CHOICE:
             radius = float(input("Enter the circle's radius: "))
             print('The area is', circle.area(radius))
+            print()
+            print()
+
         elif choice == CIRCUMFERENCE_CHOICE:
             radius = float(input("Enter the circle's radius: "))
             print('The area is',
             circle.circumference(radius))
+            print()
+            print()
+
         elif choice == AREA_RECTANGLE_CHOICE:
             width = float(input("Enter the rectangle's width: "))
             length = float(input("Enter the rectangle's length: "))
             print('The area is', rectangle.area(width, length))
+            print()
+            print()
+
         elif choice == PERIMETER_RECTANGLE_CHOICE:
             width = float(input("Enter the rectangle's width: "))
             length = float(input("Enter the rectabgle's length: "))
             print('The perimeter is',
             rectangle.perimeter(width, lenght))
+            print()
+            print()
+
         elif choice == QUIT_CHOICE:
             print('Exiting the program...')
         else:
