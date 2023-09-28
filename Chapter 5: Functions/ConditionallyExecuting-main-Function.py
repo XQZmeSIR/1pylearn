7088a1167bb77a960cf0102aa627d0e374e78e81
XQZmeSIR <rys2000lan7733@mail.ru>
2023-09-28 22:50:03 +0300

    fga

--- Chapter 5: Functions/ConditionallyExecuting-main-Function.py
+++ Chapter 5: Functions/ConditionallyExecuting-main-Function.py
@@ -22,13 +22,43 @@
 
 
 
+# def times_ten(arg):
+#     return arg * 10
+
+# print(times_ten(10))
 
 
+# value = 12
+# result = times_ten(value)
+# print(result)
 
 
+# def my_function(a, b, c):
+#     d = (a + c) / b
+#     print(d)
 
 
+# my_function(a=2, b=4, c=6)
 
+import tkinter as tk
+from tkinter import messagebox
+
+def save_note():
+    note = text.get("1.0", "end-1c")  # Get the text from the text widget
+    with open("notes.txt", "a") as file:
+        file.write(note + "\n")
+    messagebox.showinfo("Note Saved", "Your note has been saved successfully.")
+
+app = tk.Tk()
+app.title("Note-taking App")
+
+text = tk.Text(app, wrap=tk.WORD, height=10, width=40)
+text.pack()
+
+save_button = tk.Button(app, text="Save Note", command=save_note)
+save_button.pack()
+
+app.mainloop()
 
 
 
