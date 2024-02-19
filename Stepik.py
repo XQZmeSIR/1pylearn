c84e2f55b53165e2e0579ddd65767642ef3d7b1f
XQZmeSIR <rys2000lan7733@mail.ru>
2024-02-19 13:10:19 +0300

    New chapter in Gaddis

--- Stepik.py
+++ Stepik.py
@@ -1,14 +1,65 @@
-age = int(input("What's your age: "))
+# age = int(input("What's your age: "))
 
-if age <= 13:
-    print("You are a child")
-elif age >= 14 and age <= 24:
-    print("You are an aduld")
-elif age >= 24 and age <= 59:
-    print("You are a formed man")
-else:
-    print("You are an elderly")
+# if age <= 13:
+#     print("You are a child")
+# elif age >= 14 and age <= 24:
+#     print("You are an aduld")
+# elif age >= 24 and age <= 59:
+#     print("You are a formed man")
+# else:
+#     print("You are an elderly")
 
 
+
+# year = int(input())
+# # Проверяем, выполняются ли условия для високосного года:
+# # Год делится на 4 без остатка,
+# # Год не делится на 100 без остатка, за исключением случаев, когда год делится на 400 без остатка.
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+# r1 = int(input())
+# r2 = int(input())
+# c1 = int(input())
+# c2 = int(input())
+
+# if r1 == r2 or c1 == c2:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# # Получаем четыре координаты и сохраняем их в переменных x1, y1, x2, y2
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# # Вычисляем разность координат по осям x и y для определения хода короля
+# x = x2 - x1  # разность координат по оси x
+# y = y2 - y1  # разность координат по оси y
+# # так как король может ходить во все направления, но только на одну клетку.
+# if -1 <= x <= 1 and -1 <= y <= 1:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# x = int(input())
+# y = int(input())
+
+# print(f"Точка(x = {x}, y = {y})")
+
+# print(f"{x = :.3f}")
+
+marks=[1,5,4,5,6]
+print(marks[4])
+a=[]
+a.append(34)
+print(a)
+print(len(marks))
 
 
