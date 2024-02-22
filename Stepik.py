37ec4f679af5d6ba7bea8271be79c5f6a44d4a44
XQZmeSIR <rys2000lan7733@mail.ru>
2024-02-22 20:44:06 +0300

    shut down

--- Stepik.py
+++ Stepik.py
@@ -62,17 +62,12 @@
 # print(a)
 # print(len(marks))
 
-a = int(input())  
-b = int(input())
-c = int(input())    
+mass = int(input())  
 
-if a > b > c or a < b < c:
-    print(b)
-
-elif b < c < a or b > c > a:
-    print(c)
-    
-else:
-    print(a)
-
+if mass <= 59:
+    print('Легкий вес')
+elif mass<= 63:
+    print('Первый полусредний вес')
+elif mass <=68
+    print('Полусредний вес')
 
