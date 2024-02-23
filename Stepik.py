54f5741698add7862dc3a156998de9f054f663ac
XQZmeSIR <rys2000lan7733@mail.ru>
2024-02-23 15:03:31 +0300

    shut down 23 FEB 2024 | 2h 55m | stepik

--- Stepik.py
+++ Stepik.py
@@ -132,7 +132,13 @@
 # else:
 #     print("Число неинтересное")  # Выводим сообщение, если условие не выполняется
 
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
 
+print(abs(a) + abs(b) + abs(c) + abs(d) + abs(e))
 
 
 
